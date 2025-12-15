5886e99cf68d50b5dcd580ef667cfccac848dc2c
Matthew Corrales Osuna <mcorra354@west-mec.org>
2025-12-15 15:57:42 -0700

    set up commit

---
+++ server/src/server.js
@@ -0,0 +1,87 @@
+// src/server.js
+import express from "express";
+import "dotenv/config";
+import {
+  createPlayer,
+  getAllPlayers,
+  getPlayer,
+} from "./services/playerService.js";
+const app = express();
+const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || "development";
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome Home!");
+});
+
+app.get("/api/players", (req, res) => {
+  const players = getAllPlayers();
+  res.status(200).json({
+    success: true,
+    players
+  });
+});
+
+app.get("/api/players/:id", (req, res) => {
+  const player = getPlayer(req.params.id);
+  if (player.error) {
+    return res.status(player.status).json({ error: player.error });
+  }
+  res.status(200).json({
+    success: true,
+    player
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("My name is Ryan");
+});
+
+app.get("/error", (req, res) => {
+  throw new Error("error from /error route");
+});
+
+// POST ROUTES
+
+app.post("/api/players", (req, res) => {
+  try {
+    const { name, age } = req.body;
+    const trimmedName = name?.trim();
+    if (!name) {
+      return res.status(400).json({
+        error: "Name is required",
+      });
+    }
+    const player = createPlayer(trimmedName, age);
+    if (player.error) {
+      return res.status(player.status).json({ error: player.error });
+    }
+    res.status(201).json({
+      success: true,
+      player,
+    });
+    // throw new Error('sup 3pm');
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).send("The page you're looking for does not exist");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    error: "Internal Server Error! ERror!",
+    msg: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
---
+++ server/src/services/playerService.js
@@ -0,0 +1,42 @@
+// server/src/services/playerService.js
+import { v4 as uuidv4 } from "uuid";
+import db from "../config/database.js";
+
+export function createPlayer(name, age) { // why not destructure
+    const playerId = uuidv4(); 
+    const createdAt = Date.now();
+
+    try {
+        db.prepare(
+            `INSERT INTO players(id, name, age, created_at) VALUES (?, ?, ?, ?)`
+        ).run(playerId, name, age, createdAt);
+        return {
+            id: playerId,
+            name,
+            age,
+            createdAt
+        };
+    } catch (err) {
+        if (err.message.includes('UNIQUE constraint failed')) {
+            return {
+                error: 'Player name exists',
+                status: 400
+            }
+        }
+        throw err;
+    }
+}
+
+export function getAllPlayers() {
+    return db.prepare(`SELECT * FROM players`).all();
+}
+
+export function getPlayer(id) { 
+    const player = db.prepare(`SELECT * FROM players WHERE id = ?`).get(id);
+    if (!player) { 
+        return {
+            error: 'Player not found',
+            status: 400
+        }
+    }
+}
