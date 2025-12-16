b606c22671d9808b065ad4ffa716b20da10153e5
Zachary Andrews <zandre375@west-mec.org>
2025-12-16 15:19:54 -0700

    added cors for server and got rid of music in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 // src/App.jsx
 import Game from './components/Game/Game'
-let christmas = new Audio("clientsrcaudiochristmas-music-448322.mp3");
-christmas.play()
+
 function App() {
   return (
     <div className="app">
