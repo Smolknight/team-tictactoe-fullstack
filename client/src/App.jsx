d399ca21761c4d5319f8c25c79e2d46e8422cc16
Zachary Andrews <zandre375@west-mec.org>
2025-12-15 17:27:14 -0700

    added christmas music | not working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 // src/App.jsx
 import Game from './components/Game/Game'
-
+let christmas = new Audio("clientsrcaudiochristmas-music-448322.mp3");
+christmas.play()
 function App() {
   return (
     <div className="app">
       <Game />
+
     </div>
-  )
+  );
 }
 
 export default App
